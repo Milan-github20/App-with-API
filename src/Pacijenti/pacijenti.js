47138ed4caf1e08af8f9d55c2ef1ef2d6f27aa86
Milan-github20 <80204547+Milan-github20@users.noreply.github.com>
2022-09-09 14:39:24 +0200

    Add files via upload

--- src/Pacijenti/pacijenti.js
+++ src/Pacijenti/pacijenti.js
@@ -26,69 +26,71 @@ function Pacijenti(props) {
     e.preventDefault();
     if (window.confirm("Da li ste sigurni da zelite obrisati pacijenta?")) {
       e.target.parentElement.parentElement.remove();
-      console.log(id);
       axios
         .post(
           `http://172.18.1.73:8080/api3.cfc?method=pacijent_obrisi&id=${id}`
         )
         .then((res) => {
-          console.log(res.data);
           alert("Pacijent je obrisan!");
-          console.log(props.pacijenti);
         });
     }
   }
   return (
     <>
       <table className="tabela--tabela_pacijenti">
-        <tbody>
+        <thead>
           <tr className="tr--header_tr">
             <th>JMBG</th>
             <th>IME</th>
             <th>PREZIME</th>
-            <th>GRAD</th>
+            <th className="grad---">GRAD</th>
             <th></th>
             <th></th>
           </tr>
-          <hr className="hr--horizontalna_linija"></hr>
+        </thead>
+
+        <hr className="hr--horizontalna_linija"></hr>
+        <tbody>
           {props.pacijenti.map((item) => (
-            <tr key={item.id} className="tr--main_dio_tabele">
-              <th>{item.jmbg}</th>
-              <th>
-                {item.ime.charAt(0).toUpperCase() +
-                  item.ime.slice(1).toLowerCase()}
-              </th>
-              <th>
-                {item.prezime.charAt(0).toUpperCase() +
-                  item.prezime.slice(1).toLowerCase()}
-              </th>
-              <th>{item.grad}</th>
-              <th>
-                <button
-                  className="th--button_1"
-                  onClick={() => {
-                    editPacijenta(
-                      item.id,
-                      item.ime,
-                      item.prezime,
-                      item.jmbg,
-                      item.grad
-                    );
-                    setUredi(true);
-                  }}
-                >
-                  Uredi
-                </button>
-              </th>
-              <th>
-                <button
-                  onClick={(e) => deleteItem(item.id, e)}
-                  className="th--button_2"
-                >
-                  Obrisi
-                </button>
-              </th>
-            </tr>
+            <div key={item.id}>
+              <tr className="tr--main_dio_tabele">
+                <th>{item.jmbg}</th>
+                <th>
+                  {item.ime.charAt(0).toUpperCase() +
+                    item.ime.slice(1).toLowerCase()}
+                </th>
+                <th>
+                  {item.prezime.charAt(0).toUpperCase() +
+                    item.prezime.slice(1).toLowerCase()}
+                </th>
+                <th>{item.grad}</th>
+                <th>
+                  <button
+                    className="th--button_1"
+                    onClick={() => {
+                      editPacijenta(
+                        item.id,
+                        item.ime,
+                        item.prezime,
+                        item.jmbg,
+                        item.grad
+                      );
+                      setUredi(true);
+                    }}
+                  >
+                    Uredi
+                  </button>
+                </th>
+                <th>
+                  <button
+                    onClick={(e) => deleteItem(item.id, e)}
+                    className="th--button_2"
+                  >
+                    Obrisi
+                  </button>
+                </th>
+              </tr>
+            </div>
           ))}
         </tbody>
         {uredi && isEditing && (
